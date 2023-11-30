73a5041d066f72d6d26e92616055059cce135771
Harshit Singh <harshitpanu7900@gmail.com>
2023-11-30 20:57:54 +0530

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-apt-get update 
+sudo apt update 
 
-apt install openjdk-17-jre-headless -y
+sudo apt install openjdk-17-jre-headless -y
 
 wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 
-apt-get update -y
+sudo apt update
 #old version :2.414.1
-apt-get install jenkins -y
+sudo apt install -y jenkins=2.414.1
